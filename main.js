368136198b293abbdc992609dd1e0f3205deceb1
tatazw <160794642+tatazw@users.noreply.github.com>
2025-08-14 11:23:59 -0300

    Create main.js

---
+++ main.js
@@ -0,0 +1,9 @@
+# thalita-abreu-
+github
+const tempoObjetivo1 = new Date("2026-01-05T00:00:00");
+const tempoObjetivo2 = new Date("2025-12-19T00:00:00");
+const tempoObjetivo3 = new Date("2025-07-01T00:00:00");
+const tempoObjetivo4 = new Date("2025-06-21T00:00:00");
+const tempoObjetivo4 = new Date("2025-07-12T00:00:00");
+
+const tempos = [tempoObjetivo1, tempoObjetivo2, tempoObjetivo3, tempoObjetivo4]
